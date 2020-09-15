2e5e22c6ea81a0318400c757bec51986adfae8f2
Koeco97 <koeco97@gmail.com>
2020-09-15 18:11:51 +0300

    new DateTime is replaced by LocalDateTime.now()

--- src/main/java/Kafka/KafkaSpringBoot/Sender.java
+++ src/main/java/Kafka/KafkaSpringBoot/Sender.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Service
 public class Sender {
@@ -13,7 +13,7 @@ public class Sender {
     private KafkaTemplate<String, MessageSample> kafkaTemplate;
 
     public void send(MessageSample data, String topic) {
-        data.setHandledTimestamp(new Date().toString());
+        data.setHandledTimestamp(LocalDateTime.now().toString());
         ProducerRecord<String, MessageSample> record = new ProducerRecord<>(topic, data);
         kafkaTemplate.send(record);
     }
